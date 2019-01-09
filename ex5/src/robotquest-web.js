8e92b1c947d201cd94a9383cbab80250b79cf1d2
GraceHanssen <saccharin07@hotmail.com>
2019-01-09 06:39:01 +0100

    made few additional changes but cannot figure out js yet

--- ex5/src/robotquest-web.js
+++ ex5/src/robotquest-web.js
@@ -2,15 +2,14 @@
 import { turn, move } from './robotquest-features.js';
 
 
-
 function main() {
 
     let robot = {
         position: {
-            line: 3,
+            line: 0,
             column: 0
     },
-        head: 'right'
+        head: 'up'
 
     };
 
@@ -19,9 +18,9 @@ function main() {
 
     // attach events to your elements/buttons
 
-    document.getElementById("turn-left-button").addEventListener("click", function() { turn_left(); }, true);
-    document.getElementById("turn-right-button").addEventListener("click", function() { turn_right(); }, false);
-    document.getElementById("move-button").addEventListener("click", function() { move_robot(); }, false);
+    document.getElementById("turn-left-button").addEventListener("click", function() { turn_left(robot); }, false);
+    document.getElementById("turn-right-button").addEventListener("click", function() { turn_right(robot); }, false);
+    document.getElementById("move-button").addEventListener("click", function() { move_robot(robot); }, false);
 
 
     // Initialize your global variables (robot, nbOfMoves, etc...)
@@ -29,36 +28,36 @@ function main() {
 
 }
 
-function move_robot() {
+function move_robot(robot) {
     console.log("Move the robot");
 
-    const maxLineIndex = 4;
-    const maxColumnIndex= 4;
 
-    let nbOfMoves = 1;
     // call the move function from robotquest-features (already imported 4U)
     move(robot, maxLineIndex, maxColumnIndex, nbOfMoves);
+
 }
 
-function turn_left() {
+function turn_left(robot) {
     console.log("Turn left");
 
-    let turns = 2;
 
     // you can call this function directly from robotquest-features.js (see import at top of file):
     turn(robot, 'turn-left', turns);
+
 }
 
 
-function turn_right() {
+function turn_right(robot) {
     console.log("Turn right");
 
 
-    let turns = 3;
     // you can call this function directly from robotquest-features.js (see import at top of file):
     turn(robot, 'turn-right', turns);
+
 }
 
+
+}
 
 // Calling main on load
 window.onload = main;
